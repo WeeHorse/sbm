d741c11db829a1b2e48309fb5b313425fbcdd10e
Benjamin Berglund <benjaminberglund@gmail.com>
2020-09-17 11:35:23 +0200

    Added content type Insurance and testers login credentials

--- keystone.js
+++ keystone.js
@@ -57,6 +57,7 @@ keystone.set('nav', {
 	galleries: 'galleries',
 	enquiries: 'enquiries',
 	users: 'users',
+	insurances: 'insurances'
 });
 
 // Start Keystone to connect to your database and initialise the web server
---
+++ models/Insurance.js
@@ -0,0 +1,31 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+/**
+ * Insurance Model
+ * ==========
+ */
+
+var Insurance = new keystone.List('Insurance', {
+  map: { name: 'title' },
+  autokey: { path: 'slug', from: 'title', unique: true },
+});
+
+Insurance.add({
+  title: { type: String, required: true },
+  state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
+  author: { type: Types.Relationship, ref: 'User', index: true },
+  publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
+  image: { type: Types.CloudinaryImage },
+  content: {
+    brief: { type: Types.Html, wysiwyg: true, height: 150 },
+    extended: { type: Types.Html, wysiwyg: true, height: 400 },
+  }
+});
+
+Insurance.schema.virtual('content.full').get(function () {
+  return this.content.extended || this.content.brief;
+});
+
+Insurance.defaultColumns = 'title, state|20%, author|20%, publishedDate|20%';
+Insurance.register();
