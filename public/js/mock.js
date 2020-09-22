fc55cced31f645b33cc01661301798cf97dc2126
Benjamin Berglund <benjaminberglund@gmail.com>
2020-09-22 10:16:30 +0200

    matching regn and ssn + testdata in console + mock handles various formats on regnr and ssn

--- public/js/mock.js
+++ public/js/mock.js
@@ -2,35 +2,58 @@ let people = [
   {
     "firstName": "Görgen",
     "lastName": "Welin",
-    "street": "Hjalmar strååts väg",
-    "zip": "731 30",
+    "street": "Hjalmar strååts väg 28",
+    "zip": "73130",
     "muncipality": "KÖPING",
-    "ssn": "580817-5250"
+    "ssn": "195808175250"
   },
   {
     "firstName": "Esbjörn",
     "lastName": "Svanberg",
-    "street": "Holmgård",
-    "zip": "856 51",
+    "street": "Storgatan 7B",
+    "zip": "85651",
     "muncipality": "SUNDSVALL",
-    "ssn": "990921-8175"
+    "ssn": "199909218175"
+  },
+  {
+    "firstName": "Karin",
+    "lastName": "Bäck",
+    "street": "Gruvstugevägen 14",
+    "zip": "97595",
+    "muncipality": "LULEÅ",
+    "ssn": "195705047263"
   }
 ]
 
 let cars = {
-  "JOA 112": {
+  "JOA112": {
     owner: people[0],
     info: {
       make: "Ford"
     }
   },
-  "XXS 007": {
+  "XXS007": {
     owner: people[1],
     info: {
       make: "Vauxhall"
     }
+  },
+  "ABC123": {
+    owner: people[2],
+    info: {
+      make: "Mercedes"
+    }
   }
 }
+
+let testData = {}
+for (let regnr in cars) {
+  testData[regnr] = parseInt(cars[regnr].owner.ssn);
+}
+setTimeout(function () {
+  console.table(testData);
+}, 500)
+
 
 $('body').on('submit', '#mysafety-store-checkout-ssn-form', checkSsnAndRegnr)
 
@@ -39,8 +62,10 @@ function checkSsnAndRegnr(e) {
   $('.form_wrapper.checkout-page__form').show();
   let $ssn = $('.checkout-page__form__user-info>div:first-child>h3')
   let $info = $('.checkout-page__form__user-info>div:last-child>h3')
-  let ssn = $('#edit-ssn-field').val();
-  let reg = $('#edit-regnr').val();
+  $ssn.siblings().html('');
+  $info.siblings().html('');
+  let ssn = $('#edit-ssn-field').val().replace(/[\s\D]?/gi, '')
+  let reg = $('#edit-regnr').val().replace(/[\s\W]?/gi, '')
   let person;
   for (p of people) {
     if (p.ssn === ssn) {
@@ -49,11 +74,12 @@ function checkSsnAndRegnr(e) {
     }
   }
   let car = cars[reg]
-  console.log('car', car);
-  $ssn.after(`<p>${person.ssn}</p>`)
-  $info.after(`
-    <p>${person.firstName} ${person.lastName}</p>
-    <p>${person.street}</p>
-    <p>${person.zip} ${person.muncipality}</p>
-  `)
+  if (cars[reg] && cars[reg].owner === person) {
+    $ssn.after(`<p>${person.ssn}</p>`)
+    $info.after(`
+      <p>${person.firstName} ${person.lastName}</p>
+      <p>${person.street}</p>
+      <p>${person.zip} ${person.muncipality}</p>
+    `)
+  }
 }
